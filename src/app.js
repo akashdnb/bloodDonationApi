74925bb537265c0e7f8b0b8059be8cf88787a0bb
akashdnb <akashranjandnb@gmail.com>
2023-03-15 23:21:03 +0530

    modified app.js

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const app= express();
 const donarRouter = require('./routers/donarRouter');
 const cors = require('cors');
 require('./db/conn.js');
+var request = require('request');
 const port= process.env.POST || 3000;
 
 app.use(express.json())
@@ -15,3 +16,12 @@ app.use(donarRouter);
 app.listen(port, ()=>{
     console.log(`Listining to port: ${port}`);
 })
+
+
+//for other app
+
+setInterval(function(){ 
+    request.get('https://inductions2022.onrender.com/');
+  },900000) 
+  
+//900000 means 15 Minutes 
